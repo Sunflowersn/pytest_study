1d3aeca0c6fe74461502dfa09be52d102700e2df
Morozov Semyon <mor.sem.nik@gmail.com>
2026-04-08 02:47:19 +0500

    Переделал тесты из первого занятия

--- homeWork1.py
+++ homeWork1.py
@@ -1,3 +1,12 @@
-print("Hello World")
-print("Hello World")
-print("Hello World")
+from selene import browser, be, have
+
+# (опционально, но рекомендуется)
+browser.config.window_width = 1280
+browser.config.window_height = 800
+
+browser.open('https://google.com')
+
+browser.element('[name="q"]').should(be.blank).type('qa.guru').press_enter()
+
+# более стабильная проверка
+browser.element('#search').should(have.text('qa.guru'))
